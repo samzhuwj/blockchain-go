470a5faed3232b2dcc1c4ead6c3ca22476547135
samzhuwj <samzhuwj@gmail.com>
2018-06-02 19:32:33 +0800

    newproofofwork method

--- proofofwork.go
+++ proofofwork.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"math"
+	"math/big"
+)
+
 const targetBits = 24
 
 // ProofOfWork represents a proof-of-work
 type ProofOfWork struct {
 	block  *Block
 	target *big.Int
+}
+
+// NewProofOfWork builds and returns a ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
 }
