702c0d7247abf5681018f2fe3bede4c2e2efd4b7
samzhuwj <samzhuwj@gmail.com>
2018-06-04 20:00:14 +0800

    function next returns next block starting from the tip

--- blockchain.go
+++ blockchain.go
@@ -64,6 +64,27 @@ func (bc *Blockchain) Iterator() *BlockchainIterator {
 	return bci
 }
 
+// Next returns next block starting from the tip
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
+}
+
 // NewBlockchain creates a new Blockchain with genesis Block
 func NewBlockchain() *Blockchain {
 	var tip []byte
