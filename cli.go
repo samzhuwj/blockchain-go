c8e6e33d2308628bf9b1511617c6ab296337a055
samzhuwj <samzhuwj@gmail.com>
2018-06-05 20:02:54 +0800

    function addblock, printchain

--- cli.go
+++ cli.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 // CLI responsible for processing command line arguments
 type CLI struct {
 	bc *Blockchain
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
 }
 
 // Run parses command line arguments and processes commands
