e2e4ace6073a2ee94d16b85ff9e9265a6c3e3192
samzhuwj <samzhuwj@gmail.com>
2018-06-05 20:01:55 +0800

    run parses command line arguments and processes commands

--- cli.go
+++ cli.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+)
+
 // CLI responsible for processing command line arguments
 type CLI struct {
 	bc *Blockchain
+}
+
+// Run parses command line arguments and processes commands
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
 }
