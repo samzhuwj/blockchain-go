867b4d45cec3151a4851098c50ec55cfe98b3bc0
samzhuwj <samzhuwj@gmail.com>
2018-06-05 20:03:35 +0800

    function printusage, validateargs

--- cli.go
+++ cli.go
@@ -13,6 +13,19 @@ type CLI struct {
 	bc *Blockchain
 }
 
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
 func (cli *CLI) addBlock(data string) {
 	cli.bc.AddBlock(data)
 	fmt.Println("Success!")
