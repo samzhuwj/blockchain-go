f8356a6cabca555d54e35ea2c5be7e876faff128
samzhuwj <samzhuwj@gmail.com>
2018-06-07 22:24:38 +0800

    setid

--- cli.go
+++ cli.go
@@ -51,8 +51,6 @@ func (cli *CLI) Run() {
 	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
 
-	addBlockData := addBlockCmd.String("data", "", "Block data")
-
 	switch os.Args[1] {
 	case "addblock":
 		err := addBlockCmd.Parse(os.Args[2:])
--- transaction.go
+++ transaction.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
 	"fmt"
+	"log"
 )
+
+const subsidy = 10
 
 // Transaction represents a Bitcoin transaction
 type Transaction struct {
 	ID   []byte
 	Vin  []TXInput
 	Vout []TXOutput
+}
+
+// SetID sets ID of a transaction
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
 }
 
 // TXInput represents a transaction input
