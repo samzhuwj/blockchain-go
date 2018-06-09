53d8a75ee62ec32b2e94e8fa0cc9722dd9152a47
samzhuwj <samzhuwj@gmail.com>
2018-06-09 22:39:29 +0800

    iscoinbase

--- transaction.go
+++ transaction.go
@@ -17,6 +17,11 @@ type Transaction struct {
 	Vout []TXOutput
 }
 
+// IsCoinbase checks whether the transaction is coinbase
+func (tx Transaction) IsCoinbase() bool {
+	return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
+}
+
 // SetID sets ID of a transaction
 func (tx *Transaction) SetID() {
 	var encoded bytes.Buffer
