cdd60591feb4ee542c5a4ce2c25b47f90dce7d30
samzhuwj <samzhuwj@gmail.com>
2018-06-10 23:54:03 +0800

    modify inputs and outputs to use addresses

---
+++ transaction_output.go
@@ -0,0 +1,21 @@
+package main
+
+import "bytes"
+
+// TXOutput represents a transaction output
+type TXOutput struct {
+	Value      int
+	PubKeyHash []byte
+}
+
+// Lock signs the output
+func (out *TXOutput) Lock(address []byte) {
+	pubKeyHash := Base58Decode(address)
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
+	out.PubKeyHash = pubKeyHash
+}
+
+// IsLockedWithKey checks if the output can be used by the owner of the pubkey
+func (out *TXOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+	return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
