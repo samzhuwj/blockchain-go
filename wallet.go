c88b2ed6bda5a932c46d8c186301f80ace4b00c7
samzhuwj <samzhuwj@gmail.com>
2018-06-10 23:49:34 +0800

    generate an address in wallet

--- wallet.go
+++ wallet.go
@@ -4,10 +4,15 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"log"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
+const version = byte(0x00)
 const walletFile = "wallet.dat"
+const addressChecksumLen = 4
 
 // Wallet stores private and public keys
 type Wallet struct {
@@ -21,6 +26,41 @@ func NewWallet() *Wallet {
 	wallet := Wallet{private, public}
 
 	return &wallet
+}
+
+// GetAddress returns wallet address
+func (w Wallet) GetAddress() []byte {
+	pubKeyHash := HashPubKey(w.PublicKey)
+
+	versionedPayload := append([]byte{version}, pubKeyHash...)
+	checksum := checksum(versionedPayload)
+
+	fullPayload := append(versionedPayload, checksum...)
+	address := Base58Encode(fullPayload)
+
+	return address
+}
+
+// HashPubKey hashes public key
+func HashPubKey(pubKey []byte) []byte {
+	publicSHA256 := sha256.Sum256(pubKey)
+
+	RIPEMD160Hasher := ripemd160.New()
+	_, err := RIPEMD160Hasher.Write(publicSHA256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+// Checksum generates a checksum for a public key
+func checksum(payload []byte) []byte {
+	firstSHA := sha256.Sum256(payload)
+	secondSHA := sha256.Sum256(firstSHA[:])
+
+	return secondSHA[:addressChecksumLen]
 }
 
 func newKeyPair() (ecdsa.PrivateKey, []byte) {
